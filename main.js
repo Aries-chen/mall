784bd602f3862b7ade90ce54dd3f539c06cd6e90
Aries-chen <1416535234@qq.com>
2022-08-22 11:39:21 +0800

    develop home page

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 配置请求根路径
+// $http.baseUrl = 'https://www.uinav.com'
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 请求响应器
+$http.beforeRequest = function (options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
 
 Vue.config.productionTip = false
 
